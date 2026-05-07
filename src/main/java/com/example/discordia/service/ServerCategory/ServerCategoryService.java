92130a5c93f1311a372c58e19f67b94bef72acd8
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-08 02:20:22 +0800

    feat: create update and delete category endpoints

--- src/main/java/com/example/discordia/service/ServerCategory/ServerCategoryService.java
+++ src/main/java/com/example/discordia/service/ServerCategory/ServerCategoryService.java
@@ -4,11 +4,13 @@ import com.example.discordia.dto.ServerCategoryDto;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-import java.util.List;
 
 @Service
 public interface ServerCategoryService {
 
     ServerCategoryDto createCategory(ServerCategoryDto categoryDto);
     ServerCategoryDto getCategoryById(UUID categoryId);
+
+    String updateCategory(UUID categoryId, String categoryName);
+    int deleteCategory(UUID categoryId);
 }
