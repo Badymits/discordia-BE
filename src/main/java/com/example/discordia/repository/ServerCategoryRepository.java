86a2df59618ce87d6d0151a0a917ad182389431a
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-08 02:19:55 +0800

    feat: create update and delete category endpoints

--- src/main/java/com/example/discordia/repository/ServerCategoryRepository.java
+++ src/main/java/com/example/discordia/repository/ServerCategoryRepository.java
@@ -2,7 +2,10 @@ package com.example.discordia.repository;
 
 import com.example.discordia.model.ServerCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -10,4 +13,17 @@ import java.util.UUID;
 public interface ServerCategoryRepository extends JpaRepository<ServerCategory, Long> {
 
     ServerCategory findByCategoryId (UUID categoryId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE ServerCategory cat SET cat.categoryName = :categoryName " +
+            "WHERE cat.categoryId = :categoryId")
+    void updateCategory(UUID categoryId, String categoryName);
+
+    @Transactional
+    @Modifying
+    @Query(
+            "DELETE FROM ServerCategory cat WHERE cat.categoryId = :categoryId"
+    )
+    int deleteCategory(UUID categoryId);
 }
