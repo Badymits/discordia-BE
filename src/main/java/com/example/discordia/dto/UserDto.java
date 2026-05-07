e05a1bf1023e57742dd7bb737d1a63e7301bc8aa
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-08 02:28:17 +0800

    fix: update capitalization of username field

--- src/main/java/com/example/discordia/dto/UserDto.java
+++ src/main/java/com/example/discordia/dto/UserDto.java
@@ -3,7 +3,6 @@ package com.example.discordia.dto;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -16,7 +15,7 @@ public class UserDto {
     private String firstname;
     private String lastname;
 
-    private String userName;
+    private String username;
     private String displayName;
     private String userBio;
 
