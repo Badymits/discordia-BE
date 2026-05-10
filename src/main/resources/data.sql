48cac5bee00859616754ee0bf57865e78ace3e16
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-10 09:51:58 +0800

    feat: create sql file to add user instances

---
+++ src/main/resources/data.sql
@@ -0,0 +1,46 @@
+INSERT INTO discordia.users (
+    UserID,
+    FirstName,
+    LastName,
+    Username,
+    DisplayName,
+    Email_address,
+    ImgUrl,
+    UserBio
+) VALUES (
+UUID(),
+'faaa',
+'doe2',
+'username1haha',
+'d_rag',
+'email1@gmail.com',
+null,
+null
+), (
+ UUID(),
+ 'terry',
+ 'crews',
+ 'Yayuh',
+ '127xghaha',
+ 'email2@gmail.com',
+ null,
+ null
+ ), (
+   UUID(),
+   'terry4',
+   'crwww',
+   'Mord',
+   'Rigb',
+   'email3@gmail.com',
+   null,
+   null
+), (
+     UUID(),
+     'leo',
+     'cucitini',
+     'LM10',
+     'WC_winner haha',
+     'email67@gmail.com',
+     null,
+     null
+ );
