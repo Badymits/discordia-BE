7eba6205eb1deee58d67b3fb8a95555386ce9fec
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-10 09:53:34 +0800

    feat: add enable jpa auditing annotation

--- src/main/java/com/example/discordia/DiscordiaApplication.java
+++ src/main/java/com/example/discordia/DiscordiaApplication.java
@@ -2,9 +2,11 @@ package com.example.discordia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableJpaAuditing
 @RestController
 @SpringBootApplication
 public class DiscordiaApplication {
