372eb3e426b4dbab8852435159693d171b6c2c8b
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-10 09:59:49 +0800

    fix: update dto class to extend from MessageDto base class

--- src/main/java/com/example/discordia/dto/ServerMessageDto.java
+++ src/main/java/com/example/discordia/dto/ServerMessageDto.java
@@ -2,28 +2,18 @@ package com.example.discordia.dto;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class ServerMessageDto {
-
-    private String message;
-    private String displayName;
-    private String userAvatar;
-    private String messageImgUrl;
-
-    private Boolean isContentWithImg;
-    private LocalDateTime dateTimestamp;
-
-    private Boolean isReply;
-    private ReplyMessageDto repliedTo;
+public class ServerMessageDto extends MessageDto {
 
     private UUID channelId;
     private UUID userId;
-    private UUID messageId;
     private UUID serverId;
 
 }
