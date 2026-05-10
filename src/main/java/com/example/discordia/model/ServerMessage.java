aceb9fe13378db6abb69cc747c5ca5e455d68a3e
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-10 09:58:25 +0800

    feat: extend from Message Base class

--- src/main/java/com/example/discordia/model/ServerMessage.java
+++ src/main/java/com/example/discordia/model/ServerMessage.java
@@ -11,55 +11,20 @@ import org.springframework.data.annotation.CreatedDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Setter
 @Getter
-@Entity
 @ToString
-@Table(name = "server_messages")
-public class ServerMessage {
-
-    @Id
-    @Column(name = "MessageID")
-    @JdbcTypeCode(java.sql.Types.VARCHAR)
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID messageId;
-
-    @Setter
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "UserID", nullable = false)
-    private UserModel user;
-
-    @Setter
-    @Column(name = "Message", columnDefinition = "TEXT")
-    private String message;
-
-    @Setter
-    @Getter
-    @Column(name = "MessageImgUrl")
-    private String messageImgUrl;
-
-    @CreatedDate
-    @Setter
-    @Column(nullable = false, updatable = false, name = "MessageCreated")
-    private LocalDateTime dateTimestamp;
-
-    @Getter
-    @Setter
-    @Column(name = "IsContentWithImg")
-    private Boolean isContentWithImg;
-
-    @Setter
-    @Column(name = "IsReply")
-    private Boolean isReply;
+@Entity
+@Table(name = "server_message")
+public class ServerMessage extends Message {
 
     @ManyToOne
     @JoinColumn(name = "ChannelId")
-    @Setter
     private ServerChannel serverChannel;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Setter
+    @JoinColumn(name = "ServerMessageId")
     @Getter
     private ServerMessage repliedTo;
-
 
 }
