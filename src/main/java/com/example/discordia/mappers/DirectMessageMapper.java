3513f3af2c0fc86c95c711f9b7d5970ec53f1a96
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-28 01:18:02 +0800

    feat: create direct message mapper

---
+++ src/main/java/com/example/discordia/mappers/DirectMessageMapper.java
@@ -0,0 +1,23 @@
+package com.example.discordia.mappers;
+
+import com.example.discordia.dto.DirectMessageDto;
+import com.example.discordia.model.DirectMessage;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(componentModel = "spring", uses = {UserMapper.class, DirectChannelMapper.class})
+public interface DirectMessageMapper {
+
+    @Mapping(source = "user.userId", target = "userId")
+    @Mapping(source = "user.displayName", target = "displayName")
+    @Mapping(source = "user.imgUrl", target = "userAvatar")
+    @Mapping(source = "serverId", target = "serverId")
+    @Mapping(source = "directChannelModel.directChannelId", target = "directChannelId")
+    DirectMessageDto directMessageToDto(DirectMessage message);
+
+    @Mapping(source = "userId", target = "user")
+    @Mapping(source = "directChannelId", target= "directChannelModel")
+    DirectMessage dtoToDirectMessage(DirectMessageDto dto);
+}
