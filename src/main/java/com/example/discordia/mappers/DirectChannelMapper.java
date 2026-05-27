040476ebce4cf05fe66b96d7e39787a08a48eaf5
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-28 01:06:03 +0800

    feat: add map for directChannel id to entity. add Direct channel revolvers for mapper annotation

--- src/main/java/com/example/discordia/mappers/DirectChannelMapper.java
+++ src/main/java/com/example/discordia/mappers/DirectChannelMapper.java
@@ -3,15 +3,24 @@ package com.example.discordia.mappers;
 
 import com.example.discordia.dto.DirectChannelDto;
 import com.example.discordia.model.DirectChannel;
+import com.example.discordia.resolvers.DirectChannelResolvers;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = UserMapper.class)
+import java.util.UUID;
+
+@Mapper(
+        componentModel = "spring",
+        uses = {
+                UserMapper.class,
+                DirectChannelResolvers.class
+        }
+)
 public interface DirectChannelMapper {
-
-    DirectChannelMapper directChannelMapper = Mappers.getMapper(DirectChannelMapper.class);
-
 
     DirectChannelDto directChannelModelToDto(DirectChannel channel);
     DirectChannel dtoToDirectChannelModel(DirectChannelDto dto);
+
+    DirectChannel map(UUID directChannelId);
+    UUID map(DirectChannel channel);
+
 }
