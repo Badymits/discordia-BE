b3e37e92e666066baffe16b6efcaf44a54fe70d2
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-28 01:16:07 +0800

    feat: add indices, entity listender annotation for automatic date to populate on create. add dynamic update for update methods

--- src/main/java/com/example/discordia/model/DirectMessage.java
+++ src/main/java/com/example/discordia/model/DirectMessage.java
@@ -1,22 +1,37 @@
 package com.example.discordia.model;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @Setter
 @Entity
-@Table(name = "direct_message")
+@Table(name = "direct_message",
+        indexes = {
+                @Index(name = "idx_messageCreated", columnList = "MessageCreated"),
+                @Index(name = "idx_directChannelId", columnList = "DirectChannelId")
+        }
+    )
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(callSuper = true)
+@DynamicUpdate
 public class DirectMessage extends Message{
 
 
-    @JoinColumn(name = "DirectMessageId")
+    @JoinColumn(name = "DirectChannelId")
     @ManyToOne
-    private DirectChannel directMessageModel;
+    @JsonBackReference
+    private DirectChannel directChannelModel;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @Getter
     private DirectMessage repliedTo;
+
 
 }
