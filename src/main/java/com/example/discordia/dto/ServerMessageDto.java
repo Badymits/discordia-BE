4c81d85bce7e22ca128e40b440b4313e13808fd1
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-28 01:46:47 +0800

    fix: remove server id field in child dto class

--- src/main/java/com/example/discordia/dto/ServerMessageDto.java
+++ src/main/java/com/example/discordia/dto/ServerMessageDto.java
@@ -4,8 +4,6 @@ package com.example.discordia.dto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
@@ -14,6 +12,4 @@ public class ServerMessageDto extends MessageDto {
 
     private UUID channelId;
     private UUID userId;
-    private UUID serverId;
-
 }
