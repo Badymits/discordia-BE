8b058d9833057d118ed7bc287b148f0c104a690c
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-05-28 01:36:45 +0800

    feat: add userId field. add inherit to messagedto

--- src/main/java/com/example/discordia/dto/UploadImageDto.java
+++ src/main/java/com/example/discordia/dto/UploadImageDto.java
@@ -2,12 +2,16 @@ package com.example.discordia.dto;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.UUID;
 
+// generated methods will include the fields from the parent (super) class in their calculations
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class UploadImageDto {
+public class UploadImageDto extends MessageDto {
 
     private UUID messageId;
+    private UUID userId;
     private String messageImgUrl;
 }
