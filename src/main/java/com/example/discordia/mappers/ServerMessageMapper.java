fbc3de6d0600fd83ea74df2e2467b8a2f8949752
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 01:51:55 +0800

    feat: create server message to dto and vice versa mapper methods

--- src/main/java/com/example/discordia/mappers/ServerMessageMapper.java
+++ src/main/java/com/example/discordia/mappers/ServerMessageMapper.java
@@ -1,10 +1,26 @@
 package com.example.discordia.mappers;
 
 
+import com.example.discordia.dto.ServerMessageDto;
+import com.example.discordia.model.ServerMessage;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = UserMapper.class)
+@Mapper(componentModel = "spring",
+        uses = {
+            UserMapper.class,
+            ServerChannelMapper.class
+        }
+    )
 public interface ServerMessageMapper {
 
+    @Mapping(source = "user.userId", target = "userId")
+    @Mapping(source = "user.displayName", target = "displayName")
+    @Mapping(source = "user.imgUrl", target = "userAvatar")
+    @Mapping(source = "serverChannel.channelId", target = "channelId")
+    ServerMessageDto serverMessageToDto(ServerMessage message);
 
+    @Mapping(source = "userId", target = "user", qualifiedByName = "uuidToUserModel")
+    @Mapping(source = "channelId", target = "serverChannel", qualifiedByName = "uuidToChannelModel")
+    ServerMessage dtoToServerMessage(ServerMessageDto messageDto);
 }
