6c1bd66ee6c89c56b299518085278e8721409694
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 01:53:31 +0800

    feat: add delete server method. add channel id param to update message method. Update get messages by channel ID data type to Array list

--- src/main/java/com/example/discordia/service/ServerMessages/ServerMessagesService.java
+++ src/main/java/com/example/discordia/service/ServerMessages/ServerMessagesService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
@@ -17,14 +17,9 @@ public interface ServerMessagesService {
     @Transactional
     ServerMessageDto createMessage(ServerMessageDto messageDto);
     ServerMessageDto toDto (ServerMessage message);
-    List<ServerMessageDto> getMessagesByChannelId(UUID channelId);
-    ServerMessageDto updateServerMessage(UUID messageId, String message);
-    UploadImageDto uploadMessageImage(
-            UUID messageId,
-            UUID serverId,
-            UUID channelId,
-            MultipartFile image
-    );
+    ArrayList<ServerMessageDto> getMessagesByChannelId(UUID channelId);
+    ServerMessageDto updateServerMessage(UUID messageId, UUID channelId, String message);
+    void deleteServerMessage(UUID messageId, UUID channelId);
 
 
 }
