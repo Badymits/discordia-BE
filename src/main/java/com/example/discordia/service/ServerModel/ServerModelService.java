3379589e81639db1f0b2bfc75861da0a767df61d
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 02:02:00 +0800

    fix: update getServers method data type to arraylist

--- src/main/java/com/example/discordia/service/ServerModel/ServerModelService.java
+++ src/main/java/com/example/discordia/service/ServerModel/ServerModelService.java
@@ -6,7 +6,7 @@ import com.example.discordia.dto.ServerModelDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
@@ -14,12 +14,9 @@ public interface ServerModelService {
 
     ServerModelDto findByServerID(UUID id);
 
-    ServerModelDto createServer(
-            ServerModelDto createServerRequest,
-            MultipartFile image
-    );
+    ServerModelDto createServer(ServerModelDto createServerRequest, MultipartFile image);
     String updateServer(UUID serverId, ServerModelDto serverDto, MultipartFile image);
-    List<ServerModelDto> getServersByUserId(UUID userId, String username);
+    ArrayList<ServerModelDto> getServersByUserId(UUID userId, String username);
 
     String getServerCode(UUID serverId);
     ServerMetaDataDto getServerMetaData(UUID serverId);
