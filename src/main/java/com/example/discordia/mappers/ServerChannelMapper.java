5e9648695820dcf5dc05c2580867c9a4026efd7e
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 01:48:03 +0800

    feat: add collection mapping strategy param to mapper annotation. Add @named annotation to serverChannel map

--- src/main/java/com/example/discordia/mappers/ServerChannelMapper.java
+++ src/main/java/com/example/discordia/mappers/ServerChannelMapper.java
@@ -4,16 +4,25 @@ package com.example.discordia.mappers;
 import com.example.discordia.dto.ServerChannelDto;
 import com.example.discordia.model.ServerChannel;
 import com.example.discordia.resolvers.ServerChannelResolvers;
+import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
 import java.util.UUID;
 
-@Mapper(componentModel = "spring", uses={ServerChannelResolvers.class})
+@Mapper(
+        componentModel = "spring",
+        collectionMappingStrategy = CollectionMappingStrategy.TARGET_IMMUTABLE,
+        uses={
+                ServerChannelResolvers.class
+        }
+)
 public interface ServerChannelMapper {
 
 
     ServerChannel dtoToServerChannel(ServerChannelDto dto);
 
+    @Named("uuidToChannelModel")
     ServerChannel map(UUID channelId);
     UUID map(ServerChannel channel);
 }
