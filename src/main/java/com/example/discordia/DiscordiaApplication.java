88bbdda80f609a4ab1c951cba2d4b25e34a54d55
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 01:40:15 +0800

    feat: add enable caching annotation

--- src/main/java/com/example/discordia/DiscordiaApplication.java
+++ src/main/java/com/example/discordia/DiscordiaApplication.java
@@ -2,11 +2,13 @@ package com.example.discordia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableJpaAuditing
+@EnableCaching
 @RestController
 @SpringBootApplication
 public class DiscordiaApplication {
