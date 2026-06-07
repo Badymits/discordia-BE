45b8a6500995187bfdf6a0a9fe568015aef76271
Elijah Cajucom <elijah.cajucom@gmail.com>
2026-06-08 01:35:52 +0800

    feat: add recipient id field

--- src/main/java/com/example/discordia/dto/DirectMessageDto.java
+++ src/main/java/com/example/discordia/dto/DirectMessageDto.java
@@ -4,7 +4,6 @@ package com.example.discordia.dto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
@@ -14,5 +13,6 @@ public class DirectMessageDto extends MessageDto {
     private UUID directMessageId;
     private UUID directChannelId;
     private UUID userId;
+    private UUID recipientId;
 
 }
